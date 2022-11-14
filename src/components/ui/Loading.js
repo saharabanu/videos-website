d22abdde0b0083f29707fbd179d4e2bff7b89148
saharabanu <saharabanungn@gmail.com>
2022-11-14 10:48:12 +0600

    added

--- src/components/ui/Loading.js
+++ src/components/ui/Loading.js
@@ -1,19 +1,9 @@
 import React from 'react'
 
-const Loading = () => {
+export default function Loading() {
   return (
-    <div>Loading</div>
-  )
+    <div className="h-72 flex items-center justify-center">
+      <div class="w-16 h-16 border-b-2 border-gray-900 rounded-full animate-spin"></div>
+    </div>
+  );
 }
-
-export default Loading
-
-
-
-// export default function Loading() {
-//   return (
-//     <div className="h-72 flex items-center justify-center">
-//       <div class="w-16 h-16 border-b-2 border-gray-900 rounded-full animate-spin"></div>
-//     </div>
-//   );
-// }
